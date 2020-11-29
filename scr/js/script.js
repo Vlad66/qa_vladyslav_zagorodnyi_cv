e8f3b0708c3fd03799cf82a2c1b9a3f282b53d96
Vlad66 <v.zagorodnyi@ukr.net>
2020-11-29 23:23:58 +0200

    first_version

--- scr/js/script.js
+++ scr/js/script.js
@@ -1,8 +1,26 @@
-function myFunction() {
-  var x = document.getElementById("myDIV");
-  if (x.style.display === "none") {
-    x.style.display = "block";
-  } else {
-    x.style.display = "none";
+// Reference the parent element
+var main = document.getElementById('main');
+
+/* Register main to the click event
+|| when clicked ANYWHERE within main
+|| toggle() is called
+*/
+main.addEventListener('click', toggle, false);
+
+function toggle(e) {
+  /* Determine if the current element in the
+  || event chain is the anchor that was
+  || clicked.
+  */
+  if (e.target !== e.currentTarget && e.target.nodeName === "A") {
+    /* tgt is the clicked link
+    || txt is the div that follows tgt
+    */
+    var tgt = e.target;
+    var txt = tgt.nextElementSibling;
+    // Toggle classes .on and .off
+    txt.classList.toggle('on');
+    txt.classList.toggle('off');
   }
+
 }
